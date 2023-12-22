e9668a85f8c6860c664f23e90bb87836a70fb9a4
Oleksandr Fediashov <olfedias@microsoft.com>
2023-12-22 15:08:55 +0100

    chore: configure bundle size reporting on the PR (#65)

--- monosize.config.mjs
+++ monosize.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import createAzureStorage from "monosize-storage-azure";
 import path from "path";
 
 const dirname = new URL(".", import.meta.url).pathname;
@@ -7,6 +8,9 @@ const dirname = new URL(".", import.meta.url).pathname;
 /** @type {import('monosize').MonoSizeConfig} */
 const config = {
   repository: "https://github.com/microsoft/keyborg",
+  storage: createAzureStorage({
+    endpoint: "https://keyborg-bundlesize.azurewebsites.net/api/latest",
+  }),
   webpack(config) {
     config.resolve = {
       alias: {
