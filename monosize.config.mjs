694c0914249865379b7142188548267e59b34266
TristanWatanabe <tristan.watanabe@gmail.com>
2024-08-07 16:52:01 -0400

    run prettier fix

--- monosize.config.mjs
+++ monosize.config.mjs
@@ -2,7 +2,7 @@
 
 import createAzureStorage from "monosize-storage-azure";
 import path from "path";
-import webpackBundler from 'monosize-bundler-webpack';
+import webpackBundler from "monosize-bundler-webpack";
 
 const dirname = new URL(".", import.meta.url).pathname;
 
@@ -12,7 +12,7 @@ const config = {
   storage: createAzureStorage({
     endpoint: "https://keyborg-bundlesize.azurewebsites.net/api/latest",
   }),
-  bundler: webpackBundler(config => {
+  bundler: webpackBundler((config) => {
     config.resolve = {
       alias: {
         keyborg: path.resolve(dirname, "dist", "esm", "index.js"),
